2bc2b0c61f76bd2d479b6840eb3b40c46e87fe0a
Tomasz Marchewka <tomaszmarchewka@hotmail.com>
2016-11-13 11:54:36 +0100

    Add brief method, add guava library.

--- src/main/java/imal/BRIEFGenerator.java
+++ src/main/java/imal/BRIEFGenerator.java
@@ -1,6 +1,22 @@
 package imal;
 
+import boofcv.alg.feature.describe.DescribePointBrief;
+import boofcv.alg.feature.describe.brief.FactoryBriefDefinition;
+import boofcv.factory.feature.describe.FactoryDescribePointAlgs;
+import boofcv.factory.filter.blur.FactoryBlurFilter;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.feature.ScalePoint;
+import boofcv.struct.feature.TupleDesc_B;
+import boofcv.struct.image.GrayF32;
+import com.google.common.primitives.Ints;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by tomas on 31.07.2016.
@@ -8,13 +24,48 @@ import java.util.List;
 public class BRIEFGenerator {
 
     private String imagePath;
-    private List<List<Integer>> output;
+    private List<List<Integer>> descriptors;
+    private List<ScalePoint> points;
 
-    public BRIEFGenerator(String imagePath) {
+    public BRIEFGenerator(List<ScalePoint> points, String imagePath) {
         this.imagePath = imagePath;
+        this.points = points;
     }
 
-    public void generate() {}
+    public void generate() {
+        this.descriptors = new ArrayList<>();
+        BufferedImage tmp = null;
+        try {
+            tmp = ImageIO.read(new File(this.imagePath));
+        } catch (IOException e) {
+            System.out.println(e + " | " + this.imagePath + "  IOError");
+        }
+
+        GrayF32 input = ConvertBufferedImage.convertFromSingle(tmp, null, GrayF32.class);
+
+
+        DescribePointBrief<GrayF32> brief = FactoryDescribePointAlgs.brief(FactoryBriefDefinition.gaussian(new Random(123), 16, 512),
+                FactoryBlurFilter.gaussian(GrayF32.class, 0, 4));
+
+
+        brief.setImage(input);
+        TupleDesc_B f = brief.createFeature();
+
+
+        for (ScalePoint moj : this.points) {
+
+            brief.process(moj.x, moj.y, f);
+
+            int[] deskryptor = new int[520];
+
+            for (int i = 0; i < f.numBits; i++) {
+                if (f.isBitTrue(i)) {
+                    deskryptor[i] = 1;
+                }
+            }
+            this.descriptors.add(Ints.asList(deskryptor));
+        }
+    }
 
     public String getImagePath() {
         return imagePath;
@@ -22,5 +73,17 @@ public class BRIEFGenerator {
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
+    }
+
+    public List<List<Integer>> getDescriptors() {
+        return descriptors;
+    }
+
+    public List<ScalePoint> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<ScalePoint> points) {
+        this.points = points;
     }
 }
--- src/main/java/imal/Main.java
+++ src/main/java/imal/Main.java
@@ -8,11 +8,24 @@ public class Main {
         int numberOfPoints = 800;
         SURFGenerator surf = new SURFGenerator(numberOfPoints, filePath);
         surf.generate();
-        System.out.println("Number of points: " + surf.getPoints().size());
-        int pointsSize = surf.getPoints().size();
-        for(int i = 0; i < pointsSize; i++) {
-            System.out.println(i +") " + surf.getPoints().get(i));
-            System.out.println(surf.getDescriptions().get(i).value + "\n");
+
+        int surfPointsSize = surf.getPoints().size();
+        System.out.println("Number of surf points: " + surfPointsSize);
+        for(int i = 0; i < surfPointsSize; i++) {
+            System.out.println("\n" + i +") " + surf.getPoints().get(i));
+            for(double value : surf.getDescriptions().get(i).value) {
+                System.out.print(value + " ");
+            }
+        }
+        BRIEFGenerator brief = new BRIEFGenerator(surf.getPoints(), filePath);
+        brief.generate();
+        int briefPointsSize = brief.getPoints().size();
+        System.out.println("Number of brief points: " + briefPointsSize);
+        for(int i = 0; i < briefPointsSize; i++) {
+            System.out.println("\n" + i +") " + brief.getPoints().get(i));
+            for(int value : brief.getDescriptors().get(i)) {
+                System.out.print(value + " ");
+            }
         }
     }
 }
